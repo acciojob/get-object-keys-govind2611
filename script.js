5e8a0dadbf1c19922f7d3a978e19b7000fefd9fa
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-13 19:12:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: "John",
+};
+
+// Add a getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test the getKeys() method on the student object
+console.log(student.getKeys()); // Output: ["name"]
